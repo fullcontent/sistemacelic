7e50a3354fa1f4d4399fec6c280e7f53560c910b
Bruno Carvalho <bgc1988@gmail.com>
2020-01-02 22:56:27 -0300

    Ajustes de pendencias

---
+++ resources/views/admin/editar-pendencia.blade.php
@@ -0,0 +1,50 @@
+@extends('adminlte::page')
+
+
+
+@section('content')
+
+	<div class="box box-primary">
+	
+	<div class="box-header with-border">
+		<h3 class="box-title">Editar pendencia</h3>
+	</div>
+
+	
+
+{!! Form::model($pendencia,['route'=>['pendencia.update', $pendencia->id],'method'=>'put','enctype'=>'multipart/form-data']) !!}
+
+	@include('admin.partials.form-pendencia')
+	
+	
+
+      			<div class="box-footer">
+      			<a href="#" class="btn btn-default">Voltar</a>
+                
+                <button type="submit" class="btn btn-info">Cadastrar</button>
+              	</div>
+    	
+    
+	
+	{!! Form::close() !!}
+
+@endsection
+
+@section('js')
+
+<script>
+	
+	$(document).ready(function() {
+
+  	$("#emissao").datepicker();
+  	$("#vencimento").datepicker();
+  	 	
+  	$("#valor").mask('000.000.000.000.000,00', {reverse: true});
+	  	
+ 
+});
+
+
+</script>
+
+@stop
---
+++ resources/views/cliente/lista-interacoes.blade.php
@@ -0,0 +1,76 @@
+@extends('adminlte::page')
+
+
+
+
+
+@section('content')
+
+	
+	<div class="box">
+					<div class="box-header">
+					
+						<h4>Interações da O.S. </h4>
+
+					</div>
+
+
+		<ul class="timeline timeline-inverse">
+                                 
+                  
+                @foreach($interacoes as $historico)
+                  <!-- timeline item -->
+                  <li>
+                    
+                    @php
+
+                    	if($historico->user->privileges == 'cliente')
+                    	{
+                    		$label = "fa fa-user bg-aqua";
+                    	}
+                    	else
+                    	{
+                    		$label = "fa fa-copyright bg-green";
+                    	}
+
+
+                    @endphp
+                    <i class="{{$label}}"></i>
+                    
+                    <div class="timeline-item">
+                      <span class="time"><i class="fa fa-clock"></i> {{\Carbon\Carbon::parse($historico->created_at)->diffForHumans()}}</span>
+                      <h3 class="timeline-header"><a href="#">{{$historico->user->name}}</a></h3>
+
+                      <div class="timeline-body">
+                  			{{$historico->observacoes}}
+                	  </div>
+                    </div>
+                  </li>
+                  <!-- END timeline item -->
+                @endforeach
+                                 
+                  
+                </ul>
+
+                <div class="box-footer">
+                
+                <div class="box-header">
+                  
+                  {!! Form::open(['route'=>'interacao.salvar']) !!}
+                  <div class="input-group">
+                  {!! Form::text('observacoes', null, ['class'=>'form-control','id'=>'observacoes','placeholder'=>'Digite a mensagem']) !!}
+                  {!! Form::hidden('servico_id',$historico->servico_id) !!}
+                  
+                      <span class="input-group-btn">
+                        <button type="submit" class="btn btn-info btn-flat">Enviar</button>
+                      </span>
+                </div>
+                {!! Form::close() !!}   
+
+                </div>
+
+            </div>
+
+	</div>
+
+@stop
