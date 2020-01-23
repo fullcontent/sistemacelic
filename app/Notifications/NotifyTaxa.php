32f6eaa06ed7f4e1fb91e310f0f55dae54541134
Bruno Carvalho <bgc1988@gmail.com>
2020-01-23 01:13:43 -0300

    inicio de desenvolvimento de notificações

---
+++ app/Notifications/NotifyTaxa.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Queue\SerializesModels;
+
+
+
+
+class NotifyTaxa extends Notification implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+    
+    public $taxa;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($taxa)
+    {
+        //
+        $this->taxa = $taxa;
+       
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            //
+            'taxa'=>$this->taxa,
+        ];
+    }
+}
