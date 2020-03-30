11ae2529b3a66b43dbeaa58b10a7e7bd86cd0a37
Bruno Carvalho <bgc1988@gmail.com>
2020-03-30 20:04:59 -0300

    Removed Listeners, Events and Notifications

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,10 +16,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         
-        'App\Events\UserLoggedIn' => [
-            'App\Listeners\NotificacaoTaxaTomorrow',
-            'App\Listeners\NotificacaoTaxaToday',
-    ],
+       
     
     ];
 
