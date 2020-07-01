8020b129fe35cb260949e3e951cebf43e702b482
Bruno Carvalho <bgc1988@gmail.com>
2020-06-30 21:31:34 -0300

    Issue #81
    Ajuste de logo e favicons

--- config/adminlte.php
+++ config/adminlte.php
@@ -30,11 +30,11 @@ return [
     |
     */
 
-    'logo' => '<img src="http://sistemacelic.net/img/logoCastro2.png">',
+    'logo' => '<img src="http://sistemacelic.net/img/logoCelic.png">',
 
-    'login_logo' => '<img src="http://sistemacelic.net/img/logoCastro2.png">',
+    'login_logo' => '<img src="http://sistemacelic.net/img/logoCelicLogin.png">',
 
-    'logo_mini' => '<img src="http://sistemacelic.net/img/logoCastro.png">',
+    'logo_mini' => '<img src="http://sistemacelic.net/img/logoCelicMini.png">',
 
     /*
     |--------------------------------------------------------------------------
