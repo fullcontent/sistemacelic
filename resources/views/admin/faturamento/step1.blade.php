caa3b9eadfb1e2bd6644323c3ae7f28d823f6e32
Bruno Carvalho <bgc1988@gmail.com>
2022-04-18 21:47:41 -0300

    Issue #240 Buscar empresa

--- resources/views/admin/faturamento/step1.blade.php
+++ resources/views/admin/faturamento/step1.blade.php
@@ -56,12 +56,32 @@
 
 
 @section('js')
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 <script>
+
+$(document).ready(function() {
+
+$("#empresas").select2({
+	placeholder: 'Selecione a empresa',
+	allowClear: true,
+	multiple: true,
+});
+
+$("#empresas").val('').trigger('change');
+
+
+  
+
+});
+
+
 
 $('#periodo').daterangepicker();
 
@@ -80,6 +100,8 @@ $("#selectAll").click(function(){
 		});
 		
 	});
+
+	
 
 	$('#empresas').change(function () {
 
