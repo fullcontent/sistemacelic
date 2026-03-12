8a97954c5d8fcd1fede6ab81971ea3b2770311f5
Bruno Carvalho <bgc1988@gmail.com>
2026-03-12 09:21:48 -0300

    #

--- resources/views/cliente/detalhe-servico.blade.php
+++ resources/views/cliente/detalhe-servico.blade.php
@@ -7,6 +7,30 @@
 
 
 @section('content')
+
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-check"></i> Sucesso!</h4>
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('warning'))
+    <div class="alert alert-warning alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-warning"></i> Atenção!</h4>
+        {{ session('warning') }}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-ban"></i> Erro!</h4>
+        {{ session('error') }}
+    </div>
+@endif
   
   
   
